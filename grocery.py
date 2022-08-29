722df9dbf106e4f32d931c72f1433e762898fd70
Gabriela Vasconcelos <gaby@asinpa.com>
2022-08-29 12:52:28 -0500

    css and html grid system tester

---
+++ grocery.py
@@ -0,0 +1,10 @@
+meat = ["chicken", "ground beef", "steak", "salmon", "shrimp", "ham", "bacon", "turkey", "pork chops", "lamb chops"]
+fruits = ["strawberries", "bananas", "apples", "kiwis", "grapes", "oranges", "mangos", "grapefruit", ""]
+veggies = ["spinach", "carrots", "zucchini", "squash", "celary", "kale", "broccoli", "cauliflower", "bell peppers", "asparagus"]
+bakery = ["chocolate cake", "yellow cakes", "chocolate chip cookies", "apple pie", "brownies", "cupcakes", "crossaints", ""]
+
+
+
+
+
+#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
