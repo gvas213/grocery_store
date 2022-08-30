b4146567d668882368c1ee310c6804fda5d64498
Gabriela Vasconcelos <gaby@asinpa.com>
2022-08-30 12:02:20 -0500

    added sections and prices

--- grocery.py
+++ grocery.py
@@ -1,8 +1,12 @@
-meat = ["chicken", "ground beef", "steak", "salmon", "shrimp", "ham", "bacon", "turkey", "pork chops", "lamb chops"]
-fruits = ["strawberries", "bananas", "apples", "kiwis", "grapes", "oranges", "mangos", "grapefruit", ""]
-veggies = ["spinach", "carrots", "zucchini", "squash", "celary", "kale", "broccoli", "cauliflower", "bell peppers", "asparagus"]
-bakery = ["chocolate cake", "yellow cakes", "chocolate chip cookies", "apple pie", "brownies", "cupcakes", "crossaints", ""]
+meat = {"chicken" : "$2.99/lb", "1lb ground beef" : "$7.49", "steak" : "$11.99/lb", "salmon" : "$9.99/lb", "shrimp" : "$9.99/lb", "ham" : "$3.89/lb", "16 oz bacon" : "$4.79", "16 oz turkey" : "$5.99", "pork chops" : "$5.49/lb", "lamb chops" : "$19.99/lb"}
+fruits = {"strawberries" : "$2.99/lb", "bananas" : "$0.49/lb", "apples" : "$1.67/lb", "kiwis" : "$3.99/lb", "grapes" : "$1.99/lb", "oranges" : "$0.99/lb", "mangos" : "$0.79/lb", "grapefruit" : "$1.50/lb", "apricot" : "1.99/lb", "watermelon" : '$1.69/lb'}
+veggies = {"5oz spinach" : "$3.00", "16oz carrots" : "$1.19", "zucchini" : "$1.19/lb", "squash" : "$1.19/lb", "celery" : "$1.79/lb", "kale" : "$1.99/lb", "broccoli" : "$1.89/lb", "cauliflower" : "$2.59/lb", "bell peppers" : "$0.69/each", "asparagus" : "$3.19/lb"}
+bakery = {"chocolate cake" : "$6.00", "yellow cake" : "$6.00", "16ct chocolate chip cookies" : "$3.99", "10ct sugar cookies" : "$4.99", "apple pie" : "$4.49", "16oz brownies" : "$4.99", "12ct cupcakes" : "$3.50", "4ct croissants" : "$4.49", "20oz bread" : "$2.49", "8ct cinnamon rolls" : "$3.00"}
+drinks = {"2qt apple juice" : "$1.79", "2qt orange juice" : "$1.99", "1gal sweet tea" : "$2.49", "2qt lemonade" : "$2.50", "smoothies" : "$2.50", "48oz coffee" : "$6.99", "1gal water" : "$1.19", "6ct coke" : "$4.79", "6ct sprite" : "$4.79", "6ct Dr. Pepper" : "$4.79"}
+dairy = {"6ct danimals" : "$2.99", "24oz cottage cheese" : "$4.79", "8oz cheddar cheese" : "$2.49", "8oz american cheese" : "$7.99", "7oz pepper jack cheese" : "$3.00", "16oz cream cheese" : "$3.29", "32oz greek yogurt" : "$6.29", "1gal 2% milk": "$3.69", "1gal skim milk" : "$3.69", "1gal whole milk" : "$3.69"}
+snacks = {"cheetos" : "$3.00", "pretzels" : "$3.00", "salt and vinegar chips" : "$3.50", "doritos" : "$5.49", "fritos" : "$3.00", "tortilla chips" : "$4.49", "gold fish", "protein bars", "fritos", "scooby snacks"}
 
+print(fruits)
 
 
 
